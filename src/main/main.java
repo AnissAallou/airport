4b8896844aa5e8c5ccdcd71934f7a8b77962a316
anisskiller <anissaallou36@gmail.com>
2019-03-03 22:01:24 +0100

    Update main.java

--- src/main/main.java
+++ src/main/main.java
@@ -6,8 +6,8 @@ import javax.persistence.EntityManager;
 public class main {
 
 	public static void main(String[] args) {
-		EntityManager em = 	model.EntityManager.DatabaseHandle.getEntityManagerFactory();
-		System.out.println("CA MARCHE");
+		EntityManager em = model.EntityManager.DatabaseHandle.getEntityManagerFactory();
+		System.out.println("Welcome in the airport");
 
 	}
 
